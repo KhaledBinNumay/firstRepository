1f6a7e4bc67314ec685b200a4b4b4dfcc3428d1d
khaledbinnumay <khaledbinnumay@gmail.com>
2018-04-27 22:46:01 +0300

    Code convention changes

--- GameStore.java
+++ GameStore.java
@@ -2,40 +2,41 @@ import java.util.Scanner;
 public class GameStore {
 
 	Game[] games;
-	int numGames;
-    static final int MAX_SIZE=50;
+	int nbGames;
+    static final int MAX_SIZE = 50;
     
     public GameStore() {
     	games=new Game[MAX_SIZE];
-    	numGames=0;
+    	nbGames =0;
     }
 	
     public int findGameById(int id) {
-    	for(int i=0 ; i<numGames ; i++)
+    	for(int i=0 ; i<nbGames ; i++)
     		if(games[i].getId()==id)
     			return i;
     	return -1;
     }
     
     public int findGameByName(String name) {
-    	for(int i=0 ; i<numGames ; i++)
+    	for(int i=0 ; i<nbGames ; i++)
     		if(games[i].getName().equalsIgnoreCase(name))
     			return i;
     	return -1;
     }
     
     public boolean addGame(Game g) {
-    	if(numGames<MAX_SIZE) {
+    	if(nbGames<MAX_SIZE) {
     		if(findGameById(g.getId())==-1) {
-    		games[numGames]=g; numGames++;
+    		games[nbGames]=g; 
+			nbGames++;
     		return true;}}
     	return false;
     }
     
     public boolean deleteGame(Game g) {
     	if(findGameByName(g.getName())!=-1) {
-    		games[findGameByName(g.getName())]=games[--numGames];
-    	    games[numGames]=null; return true;}
+    		games[findGameByName(g.getName())]=games[--nbGames];
+    	    games[nbGames]=null; return true;}
     	return false;
     }
     
@@ -43,7 +44,7 @@ public class GameStore {
     System.out.println("id: "+games[i].getId()+"\nName: "+games[i].getName()+"\nPrice: "+games[i].getPrice());	
     }
 	
-    public int getNumGames() {
-		return numGames;
+    public int getNbGames() {
+		return nbGames;
 	}
 }
